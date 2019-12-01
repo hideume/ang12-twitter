a855c1408c8257f85e0055de08f7b643671983c6
hidehiro umedu <hide3@ac.auone-net.jp>
2019-12-01 13:51:26 +0900

    limit

--- src/app/limit/limit.component.ts
+++ src/app/limit/limit.component.ts
@@ -8,11 +8,31 @@ import { TwitterService } from '../twitter.service';
 })
 export class LimitComponent implements OnInit {
   limitText;
+  limitTable = []
 
   constructor(private twitter: TwitterService) {}
 
   ngOnInit() {
-    this.twitter.limit().subscribe(a =>this.limitText = JSON.stringify(a.data.resources,null,5));
+    this.twitter.limit().subscribe(a =>{
+      this.limitText = a.data.resources;
+      var x1 = this.limitText.statuses["/statuses/home_timeline"];
+      this.limitTable.unshift(this.settable("home_timeline",x1));
+      var x2 = this.limitText.statuses["/statuses/user_timeline"];
+      this.limitTable.unshift(this.settable("user_timeline",x2));
+      var x3 = this.limitText.users["/users/show/:id"];
+      this.limitTable.unshift(this.settable("users_show",x3));
+      var x4 = this.limitText.search["/search/tweets"];
+      this.limitTable.unshift(this.settable("search",x4));
+    });
+  }
+
+  settable(nm:string,arg){
+    var f = [];
+    f.unshift(new Date(arg.reset));
+    f.unshift(arg.remaining);
+    f.unshift(arg.limit);
+    f.unshift(nm);
+    return f;
   }
 
 }
