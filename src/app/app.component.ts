03b6e09d751a910d45570c789192e9b93a66aa66
hidehiro umedu <hide3@ac.auone-net.jp>
2021-11-13 16:29:41 +0900

    pfk test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,5 +35,7 @@ export class AppComponent implements OnInit {
     this.in1.nativeElement.value = "";
     },e=>{console.log("tweet error")});
   }
-
+  onKeypressEvent(event: any){
+    console.log(event.target.value);
+ }
 }
