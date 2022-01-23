03edc8edaa9bc2f5ee9881a42bcb3ed14fb9bcff
hidehiro umedu <hide3@ac.auone-net.jp>
2022-01-23 17:46:18 +0900

    sv.js 追加

---
+++ sv.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app  = express();
+const port = 4200;
+
+// POSTのクエリーを良い感じに処理する
+app.use(express.static('./dist/tw2'));
+
+// ルーティングの設定
+/*
+app.get("/", (req, res) =>{
+  const name = req.body.name;
+  res.send(`君の名は ${name}`);
+  console.log("/ へアクセスがありました");
+});
+*/
+
+// HTTPサーバを起動する
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
